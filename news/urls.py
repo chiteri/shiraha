a836a77caa4d2f6654edb547c799e996dd6155e7
chiteri <chiteri@geek.co.ke>
2011-11-02 22:10:13 +0300

    Included the capability of pagination on the news and events page.

--- news/urls.py
+++ news/urls.py
@@ -4,10 +4,11 @@ from shiraha.news.models import News
 # Create your views here.
 news_info_dict = {
     'queryset': News.objects.all(),
-    'date_field': 'pub_date',
+#    'date_field': 'pub_date',
 }
 
-urlpatterns = patterns('django.views.generic.date_based', 
-    (r'^$', 'archive_index', news_info_dict),
-    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', news_info_dict),
+urlpatterns = patterns('django.views.generic', 
+    #(r'^$', 'date_based.archive_index', news_info_dict),
+    (r'^$', 'list_detail.object_list', dict(news_info_dict, paginate_by=3)),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'date_based.object_detail', news_info_dict),
 )
