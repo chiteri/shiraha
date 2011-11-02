6372c2a3bc965e2d58a22f5dfefc2d0ddeb7b62a
chiteri <chiteri@geek.co.ke>
2011-11-02 19:19:30 +0300

    Added minutes of meetings to the Meeting model.

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from shiraha.meetings.models import Meeting, Minutes 
+from shiraha.meetings.models import Meeting #, Minutes 
 
 # class AttendanceInline(admin.TabularInline):
 #    model = Attendance 
@@ -7,21 +7,21 @@ from shiraha.meetings.models import Meeting, Minutes
 #    max_num = 50 
 
 class MeetingAdmin (admin.ModelAdmin): 
-    list_display = ( 'day_held', 'host', 'location', 'description' )
+    list_display = ( 'day_held', 'host', 'location', 'map' )
     # inlines = [AttendanceInline,] 
     # list_filter = ['type'] 
-    fields = ['host', 'day_held', 'location', 'slug', 'description' ]  
+    fields = ['host', 'day_held', 'location', 'map', 'slug', 'description', 'minutes' ]  
     prepopulated_fields = {"slug": ("location",)}
     list_filter = ['day_held']
     date_hierarchy = 'day_held'
     search_fields = ['host'] 
 	
-class MinutesAdmin (admin.ModelAdmin): 
-    list_display = ( 'pub_date', 'meeting' )
-    fields = ['pub_date', 'meeting', 'content' ]  
-    list_filter = ['pub_date']
-    date_hierarchy = 'pub_date'
-    search_fields = ['pub_date'] 
+#class MinutesAdmin (admin.ModelAdmin): 
+#    list_display = ( 'pub_date', 'meeting' )
+#    fields = ['pub_date', 'meeting', 'content' ]  
+#    list_filter = ['pub_date']
+#    date_hierarchy = 'pub_date'
+#    search_fields = ['pub_date'] 
 	
 admin.site.register (Meeting, MeetingAdmin)
-admin.site.register (Minutes, MinutesAdmin)
+# admin.site.register (Minutes, MinutesAdmin)
--- meetings/models.py
+++ meetings/models.py
@@ -10,11 +10,11 @@ class Meeting(models.Model):
     # host = models.ForeignKey(Member, blank=True, null=True, help_text="Which member will host or hosted the meeting.") 
     host = models.CharField(max_length=100)
     location = models.CharField(max_length=200) 
-    # type = models.IntegerField(max_length=1, choices=MEETING_TYPE_CHOICES)
+    map = models.URLField(null=True, blank=True, help_text="A map showing the location of the meeting.")
     description = models.TextField(null=True, help_text="Provide the agenda of the meeting and other information important for members.") 
     # announcement = models.TextField(blank=True, null=True) 
     slug = models.SlugField()
-    # minutes = models.TextField(null=True, blank=True)
+    minutes = models.ForeignKey(FlatPage, null=True, blank=True)
     # slug = models.SlugField(prepopulate_from=['agenda'], unique_for_date=day_held, help_text="Suggested value automatically generated from the agenda.") 
 	
     class Meta: 
@@ -27,18 +27,18 @@ class Meeting(models.Model):
         return "/meeting/%s/%s/"%(self.day_held.strftime("%Y/%b/%d").lower(), self.slug ) 
 
 # Minutes for a meeting, superceded by the minutes attribute inside the Meeting model.		
-class Minutes(models.Model): 
-    pub_date = models.DateField(help_text="The day the minutes were published.") 
+# class Minutes(models.Model): 
+#    pub_date = models.DateField(help_text="The day the minutes were published.") 
     # drafted_by = models.ForeignKey(Member, blank=True, null=True) 
-    meeting = models.ForeignKey(Meeting, related_name='the_meeting') 
-    content = models.ForeignKey(FlatPage, unique=True) 
+#    meeting = models.ForeignKey(Meeting, related_name='the_meeting') 
+#    content = models.ForeignKey(FlatPage, unique=True) 
 	
-    class Meta: 
-        ordering = ['-pub_date'] 
-        verbose_name_plural = "Minutes" 
+#   class Meta: 
+#        ordering = ['-pub_date'] 
+#        verbose_name_plural = "Minutes" 
 		
-    def __unicode__(self): 
-        return u"Minutes for %s"%self.meeting 
+#    def __unicode__(self): 
+#        return u"Minutes for %s"%self.meeting 
 	
-    def get_absolute_url(self): 
-        return "/meeting/minutes/%s/"%self.pub_date.strftime("%Y/%b/%d").lower() 
+#    def get_absolute_url(self): 
+#        return "/meeting/minutes/%s/"%self.pub_date.strftime("%Y/%b/%d").lower() 
