9ccd060de4e36a0e7dab0bbbbe766ea42512dac8
Martin Akolo <chiteri@geek.co.ke>
2011-11-05 12:16:10 +0300

    List of meetings is also paginated, can browse even past meetings.

--- meetings/urls.py
+++ meetings/urls.py
@@ -2,16 +2,20 @@ from django.conf.urls.defaults import *
 from shiraha.meetings.models import Meeting 
 
 meetings_info_dict = {
+    'queryset': Meeting.objects.all(), 	
+}
+
+urlpatterns = patterns('django.views.generic', 
+    (r'^$', 'list_detail.object_list', dict(meetings_info_dict, paginate_by=12)),
+    #(r'^(?P<object_id>\d+)/details/$', 'object_detail', info_dict), 	
+)
+
+meetings_info_dict = {
     'queryset': Meeting.objects.all(), 
     'date_field': 'day_held',	
 }
 
-urlpatterns = patterns('django.views.generic.date_based', 
-    (r'^$', 'archive_index', meetings_info_dict),
-    (r'^(?P<year>\d{4})/$', 'archive_year', meetings_info_dict ),
-    (r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', meetings_info_dict ),
-    # (r'^(?P<year>\d{4}/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', meetings_info_dict ),
-    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', 
-	meetings_info_dict ),  
-    #(r'^(?P<object_id>\d+)/details/$', 'object_detail', info_dict), 	
+urlpatterns += patterns('django.views.generic', 
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 
+	'date_based.object_detail', meetings_info_dict ), 
 )
