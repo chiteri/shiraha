19139e2bba025afd3a0770984e66d6686b70536d
Martin Akolo <chiteri@geek.co.ke>
2011-11-22 20:52:53 +0300

    Incorporated maps from Google showing meeting venue on the meeting details.

--- views.py
+++ views.py
@@ -1,25 +1,10 @@
-from django.shortcuts import render_to_response
 from django.views.generic.simple import direct_to_template
-from django.http import HttpResponse 
 from django.template import RequestContext 
 from shiraha.context_processors import custom_proc
-from shiraha.news.models import News
-from shiraha.project.models import Project
-import datetime 
 
-def current_datetime(request): 
-    now = datetime.datetime.now() 
-    html = "<html><body> <h3> It is now %s. </h3> </body></html>"%now 
-    return HttpResponse (html)
-	
 def home(request): 
-    news = News.objects.all()[:4]
-    # return render_to_response('base.html', {'news':news})
-    # return direct_to_template(request, 'base.html' , {'context_instance':RequestContext(request), 'news':news}) 
     return direct_to_template(request, 'base.html' , context_instance=RequestContext(request, processors = [custom_proc]))
 	
 def gallery(request): 
-    news = News.objects.all()[:4]
-    # return render_to_response('base.html', {'news':news})
-    return direct_to_template(request, 'photo/gallery.html' , {'context_instance':RequestContext(request), 'news':news}) 
+    return direct_to_template(request, 'photo/gallery.html' , {'context_instance':RequestContext(request)}) 
 	
